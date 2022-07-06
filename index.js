c371ea9bb7caffc3f45fc04fbb23bc63a69ab791
nicobuch <nicolasbuchamer@gmail.com>
2022-07-06 10:57:22 -0300

    tercera subida

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let titulo = document.getElementById("titulo")
+/*let titulo = document.getElementById("titulo")
 console.log (titulo.innerText)
 titulo.innerText= "Segunda Entrega del Proyecto Final"
 console.log (titulo.innerText);
@@ -7,22 +7,22 @@ let container = document.getElementById("subtitulo");
 container.innerHTML = "<h2>Distintos tipos de Bebidas que Vendemos</h2>"
 container.className = "subtitulo"
 console.log (document.getElementsByClassName("subtitulo"));
-console.log (container);
+console.log (container);*/
 
-let contenedorProducto = document.getElementById("contenedorProducto")
-let contenedorChango = document.getElementById("contenedorChango")
+let contenedorProducto = document.getElementById("container");
+let contenedorChango = document.getElementById("container_chango");
 let chango = [];
 
 cargarChango();
 mostrarProductos();
 mostrarChango();
-contenedorChango();
+contfiltroProducto();
 
 
 
 function cargarChango() {
-    if(localStorage.getItem("StorageChango" !== null)){
-        chango=JSON.parse(localStorage.getItem("StorageChango"));
+    if(localStorage.getItem("StorageChango") !== null){
+        chango = JSON.parse(localStorage.getItem("StorageChango"));
         return;
     }else {
         localStorage.setItem("StorageChango", JSON.stringify(chango));
@@ -30,20 +30,40 @@ function cargarChango() {
     }
 }
 
-function mostrarProductos(){
-    producto.forEach((producto)=>{
+function mostrarProductos() {
+    producto.forEach((producto)=> {
         contenedorProducto.innerHTML += `
         <div class="tarjeta">
+        <div>
+        <h4> ID: ${producto.id}</h4>
         <p> Producto: ${producto.nombre}</p>
         <b> $ ${producto.precio}</b>
-        <h4> ID: ${producto.id}</h4>
         <button onclick="agregadoChango(${producto.id})">Comprar</button>
+        </div>
         </div>`;
-    }
-    )
+    });
 }
-function mostrarChango(){
-    chango.forEach((chango)=>{
+
+function filtroProducto() {
+    const entrada = document.querySelector(".buscarProducto");
+    entrada.addEventListener("keyup", (e) => {
+      console.log(e);
+  
+      let entradaMinuscula = e.target.value.toLowerCase();
+      console.log(entradaMinuscula);
+      if (e.target.matches(".buscarProducto")) {
+        document.querySelectorAll(".card").forEach(
+          (el) =>
+            el.textContent.toLowerCase().includes(entradaMinuscula) 
+              ? el.classList.remove("filtrar") 
+              : el.classList.add("filtrar")
+        );
+      }
+    });
+  }
+
+function mostrarChango () {
+    chango.forEach((chango) => {
         contenedorChango.innerHTML += `
         <div class="tarjeta">
         <p> Producto: ${chango.nombre}</p>
@@ -51,16 +71,15 @@ function mostrarChango(){
         <h4> ID: ${chango.id}</h4>
         <button onclick="quitarChango(${chango.id})">Quitar Chango</button>
         </div>`;
-    }
-    )
+    });
 }
 
 function contenedorChango(identificador){
-    let indice = identificador -1
-    let objeto_seleccionado = {}
-    objeto_seleccionado = producto[indice]
-    if (!chango.some(e=>e.id===identificador)){
-        chango.push(objeto_seleccionado)
+    let indice = identificador -1;
+    let objeto_seleccionado = {};
+    objeto_seleccionado = producto [indice];
+    if (!chango.some(e=>e.id===identificador)) {
+        chango.push(objeto_seleccionado);
         localStorage.setItem("StorageChango", JSON.stringify(chango));
         location.reload()
     }else {
@@ -69,8 +88,8 @@ function contenedorChango(identificador){
 }
 
 function quitarChango(id){
-    let.chango_filtrado = chango.filter((elemento)=>elemento.id != id);
-    chango=chango_filtrado;
+    let chango_filtrado = chango.filter((elemento) => elemento.id != id);
+    chango = chango_filtrado;
     localStorage.setItem("StorageChango", JSON.stringify(chango));
-    location.reload()
+    location.reload();
 }
--- productos.js
+++ productos.js
@@ -1,38 +1,62 @@
 const producto =  [
-    {id:1,
-    nombre: "Cerveza Lata Stella Artois", 
-    precio: 2900},
-    {id:2,
-    nombre: "Cerveza Lata Heineken",
-    precio: 3900},
-    {id:3,
-    nombre: "Cerveza Porron Corona 330 ml", 
-    precio: 4600},
-    {id:4, 
-    nombre: "Cerveza Corona 710ml", 
-    precio: 3400},
-    {id:5, 
-    nombre: "Cordero con piel de lobo - Malbec", 
-    precio: 2800},
-    {id:6, 
-    nombre: "Portillo - Malbec", 
-    precio: 3000},
-    {id:7, 
-    nombre: "Trumpeter-Malbec", 
-    precio: 5500},
-    {id:8, 
-    nombre: "Nicasia - Malbec", 
-    precio: 6200},
-    {id:9, 
-    nombre: "Johnnie Walker - Red Label Litro", 
-    precio: 4500},
-    {id:10, 
-    nombre: "Johnnie Walker - Black Label Litro", 
-    precio: 6500},
-    {id:11, 
-    nombre: "Fernet Branca - 750ml", 
-    precio: 1100},
-    {id:12, 
-    nombre: "Campari bitter - 750 ml", 
-    precio: 900}
-]
+    {
+        id:1,
+        nombre: "Cerveza Lata Stella Artois", 
+        precio: 2900
+    },
+    {
+        id:2,
+        nombre: "Cerveza Lata Heineken",
+        precio: 3900
+    },
+    {
+        id:3,
+        nombre: "Cerveza Porron Corona 330 ml", 
+        precio: 4600
+    },
+    {
+        id:4, 
+        nombre: "Cerveza Corona 710ml", 
+        precio: 3400
+    },
+    {
+        id:5, 
+        nombre: "Cordero con piel de lobo - Malbec", 
+        precio: 2800
+    },
+    {
+        id:6, 
+        nombre: "Portillo - Malbec", 
+        precio: 3000
+    },
+    {   
+        id:7, 
+        nombre: "Trumpeter-Malbec", 
+        precio: 5500
+    },
+    {
+        id:8, 
+        nombre: "Nicasia - Malbec", 
+        precio: 6200
+    },
+    {
+        id:9, 
+        nombre: "Johnnie Walker - Red Label Litro", 
+        precio: 4500
+    },
+    {
+        id:10, 
+        nombre: "Johnnie Walker - Black Label Litro", 
+        precio: 6500
+    },
+    {
+        id:11, 
+        nombre: "Fernet Branca - 750ml", 
+        precio: 1100
+    },
+    {
+        id:12, 
+        nombre: "Campari bitter - 750 ml", 
+        precio: 900
+    }
+];
